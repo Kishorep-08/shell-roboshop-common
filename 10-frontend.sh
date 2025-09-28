fbe15ef5b55cf1e37ac63442afa6fda148321825
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-28 14:20:47 +0530

    roboshop-common

--- 10-frontend.sh
+++ 10-frontend.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-source ./common.sh
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shell-script-logs"
+SCRIPT_NAME=$(echo "$0" | awk -F. '{print$1}')
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MYSQL_HOST=mysql.kishore-p.space
+START_TIME=$(date +%s)
 
-check_root
-app_name=frontend
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error $N: Install with root privileges" | tee -a $LOG_FILE
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R Error $N: $2 got failed" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2......$G Success $N" | tee -a $LOG_FILE
+    fi
+}
+
+
 
 ################ Creating System user ################
 
@@ -42,6 +70,9 @@ rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx configuration"
 
-systemctl restart nginx
+systemctl restart nginx 
 
-print_total_time
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+
+echo -e  "Script Executed in $Y $TOTAL_TIME seconds $N"
