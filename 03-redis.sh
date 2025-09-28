1e64a4dccbb6251eb755812f6f0e232ea4039fb4
kishorep-08 <kishore.pkdp@gmail.com>
2025-09-28 11:54:58 +0530

    roboshop-common

---
+++ 03-redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./common.sh
+
+app_name=redis
+
+check_root
+
+########## Redis Installation & Setup ###########
+dnf module disable redis -y &>> $LOG_FILE
+VALIDATE $? "Disabling default redis"
+
+dnf module enable redis:7 -y &>> $LOG_FILE
+VALIDATE $? "Enabling default redis"
+
+dnf install redis -y &>> $LOG_FILE
+VALIDATE $? "Installing default redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+
+VALIDATE $? "Allowing all remote connections"
+
+systemctl enable redis &>> $LOG_FILE
+VALIDATE $? "Enabling  redis"
+systemctl start redis &>> $LOG_FILE
+VALIDATE $? "Starting redis"
+
+print_total_time
